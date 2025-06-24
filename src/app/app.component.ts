f814e4fd7dddfa82b48affb0110dc4e25ad44898
Tihomir Denev <tdenev22@abv.bg>
2025-06-24 15:27:31 +0300

    add home route when click on title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToastModule],
+  imports: [RouterOutlet, ToastModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
